c169e2eaa5922c0dd3a9bb1c70e2607739d9cc2d
Gerrie Swart <g.swart@vehikl.com>
2020-02-07 16:02:33 -0500

    Spike: some manual testing handling

--- src/components/modeler/setUpSelectionBox.js
+++ src/components/modeler/setUpSelectionBox.js
@@ -1,5 +1,14 @@
 import { shapes } from 'jointjs';
 import store from '@/store';
+import {
+  alignBottom,
+  alignLeft,
+  alignRight,
+  alignTop,
+  centerX,
+  centerY,
+  distributeVertically,
+} from '@/components/nodes/utilities/AlignShapes';
 
 const shapesToNotTranslate = [
   'processmaker.modeler.bpmn.pool',
@@ -35,6 +44,32 @@ export default function setUpSelectionBox(setCursor, resetCursor, paperManager, 
   let selectionBox;
 
   function shiftKeyDownListener(event) {
+
+    const highlightedShapes = store.getters.highlightedShapes;
+    switch (event.key) {
+      case 'l':
+        alignLeft(highlightedShapes);
+        return;
+      case 'r':
+        alignRight(highlightedShapes);
+        return;
+      case 't':
+        alignTop(highlightedShapes);
+        return;
+      case 'b':
+        alignBottom(highlightedShapes);
+        return;
+      case 'x':
+        centerX(highlightedShapes);
+        return;
+      case 'y':
+        centerY(highlightedShapes);
+        return;
+      case 'X':
+        distributeVertically(highlightedShapes);
+        return;
+    }
+
     if (event.key !== 'Shift' || shiftKeyPressed) {
       return;
     }
